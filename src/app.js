094f7d2279fcbf06d13ebf1176d56077995d7da9
Vadasz Laszlo <maxinteger@gmail.com>
2014-11-25 20:05:45 +0100

    Hello world

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
+const React = require('react');
 import {Person} from './model/Person';
 
 global.app = function () {
     var christoph = new Person('Christoph', 'Burgdorf');
     console.log(christoph.fullName);
 };
+
+(function(){
+
+	var hello = React.createClass({
+		render: function() {
+			return React.DOM.div({}, 'hello ' + this.props.name);
+		}
+	});
+	hello = React.createFactory(hello);
+
+	React.render(hello({name: 'World'}), document.body);
+})();
