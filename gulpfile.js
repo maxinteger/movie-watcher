836f319115fa4528b56ca987e42fffe2b573545e
Vadasz Laszlo <maxinteger@gmail.com>
2014-11-26 21:42:01 +0100

    add gulp JSX compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
-var filter = require('gulp-filter');
-var to5 = require('gulp-6to5');
-
-var rimraf = require('rimraf');
+var es = require('event-stream'),
+	gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	browserify = require('gulp-browserify'),
+	filter = require('gulp-filter'),
+	to5 = require('gulp-6to5'),
+	jsx = require('gulp-jsx'),
+	rimraf = require('rimraf');
 
 var path = {
-  src: './src/**/*.js'
+  src: {
+	  all: './src/**/*.js*',
+	  js: './src/**/*.js',
+	  jsx: './src/**/*.jsx'
+  }
 };
 
 // clean the output directory
 gulp.task('clean', function(cb){
-    rimraf('compiled', cb);
+    rimraf('dist', cb);
 });
 
 // TRANSPILE ES6
 gulp.task('build', ['clean'], function() {
-  gulp.src(path.src)
-      .pipe(to5())
+	es.merge(
+		gulp.src(path.src.js).pipe(to5()),
+		gulp.src(path.src.jsx).pipe(jsx()).pipe(to5())
+	)
       .pipe(gulp.dest('compiled/src'))
       .pipe(browserify())
       .pipe(filter('app.js'))
@@ -28,7 +34,7 @@ gulp.task('build', ['clean'], function() {
 
 // WATCH FILES FOR CHANGES
 gulp.task('dev', ['clean', 'build', 'server'], function() {
-  gulp.watch(path.src, ['build']);
+  gulp.watch(path.src.all, ['build']);
 });
 
 // WEB SERVER
