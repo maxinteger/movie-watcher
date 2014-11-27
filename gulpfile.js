22471995dc5b5bfb17a112bbc47fa4f4b257023f
Vadasz Laszlo <maxinteger@gmail.com>
2014-11-27 21:15:37 +0100

    add bootstrap less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var es = require('event-stream'),
-	gulp = require('gulp'),
+var gulp = require('gulp'),
+	less = require('gulp-less'),
 	connect = require('gulp-connect'),
 	browserify = require('gulp-browserify'),
 	filter = require('gulp-filter'),
@@ -8,39 +8,48 @@ var es = require('event-stream'),
 	rimraf = require('rimraf');
 
 var path = {
-  src: {
-	  all: './src/**/*.js*',
-	  js: './src/**/*.js',
-	  jsx: './src/**/*.jsx'
-  }
+	src: {
+		js: './src/**/*.js',
+		less: './less/main.less',
+		lessAll: './less/**/*.less'
+	},
+	dist: 'compiled/'
 };
 
 // clean the output directory
 gulp.task('clean', function(cb){
-    rimraf('dist', cb);
+	return rimraf('compiled-', cb);
 });
 
-// TRANSPILE ES6
-gulp.task('build', ['clean'], function() {
-	es.merge(
-		gulp.src(path.src.js).pipe(to5()),
-		gulp.src(path.src.jsx).pipe(jsx()).pipe(to5())
-	)
-      .pipe(gulp.dest('compiled/src'))
-      .pipe(browserify())
-      .pipe(filter('app.js'))
-      .pipe(gulp.dest('compiled/combined'));
+gulp.task('build-less', function(){
+	return gulp.src(path.src.less)
+		.pipe(less())
+		.pipe(gulp.dest(path.dist + 'css'));
+});
+
+gulp.task('build-js', function() {
+	gulp.src(path.src.js)
+		.pipe(jsx())
+		.pipe(to5())
+		.pipe(gulp.dest('compiled/src'))
+		.pipe(browserify({
+			insertGlobals: true,
+			debug: true
+		}))
+		.pipe(filter('app.js'))
+		.pipe(gulp.dest(path.dist + 'combined'));
 });
 
 // WATCH FILES FOR CHANGES
-gulp.task('dev', ['clean', 'build', 'server'], function() {
-  gulp.watch(path.src.all, ['build']);
+gulp.task('dev', ['clean', 'build-js', 'build-less', 'server'], function() {
+	gulp.watch(path.src.js, ['build-js']);
+	gulp.watch(path.src.lessAll, ['build-less']);
 });
 
 // WEB SERVER
 gulp.task('server', connect.server({
-  root: [__dirname],
-  port: 8000,
-  open: true,
-  livereload: false
+	root: [__dirname],
+	port: 8000,
+	open: true,
+	livereload: false
 }));
