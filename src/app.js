98197cdcf17b97b5ec85a58ffb3053844e70d8b0
Vadasz Laszlo <maxinteger@gmail.com>
2014-12-01 19:33:21 +0100

    create example wiki search

--- src/app.js
+++ src/app.js
@@ -1,29 +1,52 @@
 const Cycle = require('cyclejs');
+const jQuery = require('jquery');
+
+var log = console.log.bind(console);
+var h = Cycle.h;
+
+function getSuggestionsFromWikipedia(text) {
+	return jQuery.ajax({
+		url: 'http://en.wikipedia.org/w/api.php',
+		dataType: 'jsonp',
+		data: {
+			action: 'opensearch',
+			format: 'json',
+			search: encodeURI(text)
+		}
+	}).promise();
+}
+
 
 var HelloModel = Cycle.createModel(['changeName$'], function (intent) {
 	return {
-		name$: intent.changeName$.startWith('')
+		name$: intent.changeName$
+			.debounce(400)
+			.flatMap(getSuggestionsFromWikipedia)
+			.map((res) => res[1])
+			.startWith([])
 	};
 });
+
 
 var HelloView = Cycle.createView(['name$'], function (model) {
 	return {
 		vtree$: model.name$
 			.map(function (name) {
-				return Cycle.h('div', {}, [
-					Cycle.h('label', 'Name:'),
-					Cycle.h('input', {
+				return h('div', {}, [
+					h('label', 'Name:'),
+					h('input', {
 						'attributes': {'type': 'text'},
 						'ev-input': 'inputText$'
 					}),
-					Cycle.h('hr'),
-					Cycle.h('h1', 'Hello ' + name)
+					h('hr'),
+					h('ul', name.map(function(item){
+						return h('li', item);
+					}))
 				]);
 			}),
 		events: ['inputText$']
 	};
 });
-
 var HelloIntent = Cycle.createIntent(['inputText$'], function (view) {
 	return {
 		changeName$: view.inputText$
